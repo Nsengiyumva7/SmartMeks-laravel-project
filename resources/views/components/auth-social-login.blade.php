fd1ba631f1a0e7bbe3e907b1f0fe19c232220378
Nsengiyumva7 <nsengiyumvaeugene01@gmail.com>
2022-12-16 20:39:00 +0200

    smart commit

---
+++ resources/views/components/auth-social-login.blade.php
@@ -0,0 +1,23 @@
+@if(env('FACEBOOK_ACTIVE') || env('GITHUB_ACTIVE') || env('GOOGLE_ACTIVE'))
+<div class="mb-4">
+    <div class="text-center mt-2 mb-4">
+        Sign in with social profiles
+    </div>
+
+    <div class="pb-4 text-center">
+        @if(env('FACEBOOK_ACTIVE'))
+        <x-button-a href="{{route('social.login', 'facebook')}}" class="bg-blue-600 hover:bg-blue-700">
+            <span class="">Facebook</span>
+        </x-button-a>
+        @endif
+
+        @if(env('GOOGLE_ACTIVE'))
+        <x-button-a href="{{route('social.login', 'google')}}" class="bg-red-600 hover:bg-red-700">
+            <span class="">Google</span>
+        </x-button-a>
+        @endif
+    </div>
+
+    <hr>
+</div>
+@endif
---
+++ resources/views/frontend/terms.blade.php
@@ -0,0 +1,26 @@
+@extends('frontend.layouts.app')
+
+@section('title') Terms &amp; Conditions - {{ config('app.name') }} @endsection
+
+@section('content')
+<div class="page-header page-header-small clear-filter" filter-color="orange">
+    <div class="page-header-image" data-parallax="true" style="background-image:url('{{asset('img/cover-01.jpg')}}');">
+    </div>
+    <div class="container">
+        <h3 class="title">
+            Terms &amp; Conditions - {{ config('app.name') }}
+        </h3>
+    </div>
+</div>
+
+<div class="section">
+    <div class="container text-center">
+        <strong>Terms &amp; Conditions</strong>
+        <p>
+        This is smartMeks platform Intended to help users people around Rwanda.
+        </p> 
+    
+</div>
+</div>
+
+@endsection
